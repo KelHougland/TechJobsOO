c2dbf907412cc3150dbf32c86faf1879b6639fcd
Kelly Hougland <knhougland@gmail.com>
2017-12-07 17:54:26 -0600

    Finished 3-5

--- src/TechJobs/ViewModels/NewJobViewModel.cs
+++ src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -15,7 +15,19 @@ namespace TechJobs.ViewModels
         [Display(Name = "Employer")]
         public int EmployerID { get; set; }
 
-        // TODO #3 - Included other fields needed to create a job,
+        [Required]
+        [Display(Name = "Location")]
+        public int LocationID { get; set; }
+
+        [Required]
+        [Display(Name = "CoreCompetency")]
+        public int CoreCompetencyID { get; set; }
+
+        [Required]
+        [Display(Name = "PositionType")]
+        public int PositionTypeID { get; set; }
+
+        // #3 - Included other fields needed to create a job,
         // with correct validation attributes and display names.
 
         public List<SelectListItem> Employers { get; set; } = new List<SelectListItem>();
@@ -36,7 +48,35 @@ namespace TechJobs.ViewModels
                 });
             }
 
-            // TODO #4 - populate the other List<SelectListItem> 
+            foreach (Location field in jobData.Locations.ToList())
+            {
+                Locations.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (CoreCompetency field in jobData.CoreCompetencies.ToList())
+            {
+                CoreCompetencies.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (PositionType field in jobData.PositionTypes.ToList())
+            {
+                PositionTypes.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+
+            // #4 - populate the other List<SelectListItem> 
             // collections needed in the view
 
         }
